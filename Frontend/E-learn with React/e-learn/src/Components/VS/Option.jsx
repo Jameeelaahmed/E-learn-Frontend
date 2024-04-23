db1adc0eb8be5f7bffdb2366f1574ac0f021a995
Jameeelaahmed <gamilas320@gmail.com>
2024-04-24 01:03:28 +0200

    update

--- Frontend/E-learn with React/e-learn/src/Components/VS/Option.jsx
+++ Frontend/E-learn with React/e-learn/src/Components/VS/Option.jsx
@@ -1,16 +1,13 @@
 import classes from './AddVsModal.module.css';
 import { useTranslation } from 'react-i18next';
-import { useRef,forwardRef } from 'react';
-const Option =forwardRef(function Option({ index,value,onChange },ref) {
+
+export default function Option({ index,onBlur }) {
     const { t } = useTranslation();
     const optionNumber = index + 1;
 
-    const inputRef = useRef(null);
-
-    const handleChange = () => {
-        onChange(index, inputRef.current.value);
-    };
-
+    function handleBlur(e){
+        onBlur(e.target.value);
+    }
 
     return (
         <div className={classes.option}>
@@ -20,14 +17,10 @@ const Option =forwardRef(function Option({ index,value,onChange },ref) {
                     type="text"
                     name={`option-${index}`}
                     id={`option-${index}`}
-                    ref={ref ? ref : inputRef}
-                    value={value}
-                    onChange={handleChange}
                     dir='auto'
+                    onBlur={handleBlur}
                 />
             </div>
         </div>
     )
-})
-
-export default Option;
+}
--- Frontend/E-learn with React/e-learn/src/Components/VS/Question.jsx
+++ Frontend/E-learn with React/e-learn/src/Components/VS/Question.jsx
@@ -1,11 +1,12 @@
 import classes from './AddVsModal.module.css'
 import InputContainer from "./InputContainer"
 import { useTranslation } from 'react-i18next';
-export default function Question({children}) {
+export default function Question({children,onLoseFocus}) {
     const { t } = useTranslation();
+    
     return (
         <div className={classes.question}>
-            <InputContainer label={t("survey-description")} type="text" nameFor="description" />
+            <InputContainer onLoseFocus={onLoseFocus} label={t("survey-description")} type="text" nameFor="description" />
             {children}
         </div>
     )
