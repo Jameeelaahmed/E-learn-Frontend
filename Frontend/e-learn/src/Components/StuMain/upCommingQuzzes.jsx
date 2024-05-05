62a1546726d2e5172316b037b54b1fe893fd67d6
Jameeelaahmed <gamilas320@gmail.com>
2024-05-06 02:37:59 +0300

    update

--- Frontend/e-learn/src/Components/StuMain/upCommingQuzzes.jsx
+++ Frontend/e-learn/src/Components/StuMain/upCommingQuzzes.jsx
@@ -1,12 +1,14 @@
 import classes from './upCommingQuiz.module.css'
-import img from '../../assets/wavedline.png'
+import { useTranslation } from 'react-i18next'
 export default function UpCommingQuizzes(){
+    const {t}=useTranslation()
     return(
         <div className={classes.UpCommingQuizzes}>
             <div className={classes.circle_one}></div>
             <div className={classes.circle_two}></div>
-            <p className={classes.group_name}>Group-name</p>
+            <p className={classes.group_name}>{t("Group")}</p>
             <div className={classes.name}>
+                {/* NAME HERE */}
                 <p>name</p>
             </div>
         </div>
