ffbccf36fbe9f6eca345b097d880e5108e7f5a66
Jameeelaahmed <gamilas320@gmail.com>
2024-05-21 15:24:57 +0300

    auth routes fixed

--- e-learn/src/utils/privateRoute.jsx
+++ e-learn/src/utils/privateRoute.jsx
@@ -3,8 +3,6 @@
 import { Outlet, Navigate } from 'react-router-dom'
 import useAuth from './useAuth'
 
-// console.log(<Outlet></Outlet>)
-
 function PrivateRoutes() {
     const token = useAuth()
     return token ? <Outlet /> : <Navigate to='/auth' />
