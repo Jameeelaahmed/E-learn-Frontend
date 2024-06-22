ee3b425b020a41b20485114109d49a3106d133af
Jameeelaahmed <gamilas320@gmail.com>
2024-06-23 01:44:10 +0300

    correct option , start date , quiz route for student DONE

--- e-learn/src/Components/QuizSurvey/Question.jsx
+++ e-learn/src/Components/QuizSurvey/Question.jsx
@@ -2,12 +2,16 @@ import classes from './AddQSModal.module.css'
 import { useTranslation } from 'react-i18next';
 import { log } from '../../log';
 import { memo, useCallback } from 'react';
+import { useLocation } from "react-router-dom";
+
 const Question = memo(function Question({ children, onLoseFocus }) {
     log('<Questionnnn /> rendered');
     const { t } = useTranslation();
     const handleBlur = useCallback((e) => {
         onLoseFocus(e.target.value);
     }, []);
+    const location = useLocation();
+    const path = location.pathname;
 
     return (
         <div className={classes.question}>
@@ -15,10 +19,12 @@ const Question = memo(function Question({ children, onLoseFocus }) {
                 <label htmlFor="description">{t("question-title")}</label>
                 <input onBlur={handleBlur} type="text" name="description" dir='auto' />
             </div>
-            <div className={classes.mark}>
-                <label htmlFor="mark">{t("mark")}</label>
-                <input onBlur={handleBlur} type="number" name="mark" dir='auto' />
-            </div>
+            {path.includes("/quizzes") &&
+                <div className={classes.mark}>
+                    <label htmlFor="mark">{t("mark")}</label>
+                    <input onBlur={handleBlur} type="number" name="mark" dir='auto' />
+                </div>
+            }
 
             {children}
         </div>
